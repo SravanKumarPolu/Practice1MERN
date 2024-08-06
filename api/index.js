e33f08da0ed4c651d8d4929b550060a1307f5e38
PVSKR <skr.polu@hotmail.com>
2024-08-06 23:29:26 +0530

    mongodb database connected to file

--- api/index.js
+++ api/index.js
@@ -6,3 +6,8 @@ const multer = require("multer");
 const app = express;
 app.use(cors());
 const PORT = 5038;
+
+const CONNECTION_STRING =
+  "mongodb+srv://admin:Yrskrmsr@cluster0.nwljkhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_NAME = "imagedb";
+let database;
