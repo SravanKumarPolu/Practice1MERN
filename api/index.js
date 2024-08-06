bea715cdf7a206a702d4875ce66096fb358797a0
PVSKR <skr.polu@hotmail.com>
2024-08-07 00:00:03 +0530

    mongodb connect successfully
     In this case node index.js(command) is used to check whether it connects successfully or not

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
 const multer = require("multer");
 
-const app = express;
+const app = express();
 app.use(cors());
 const PORT = 5038;
 
@@ -11,3 +11,15 @@ const CONNECTION_STRING =
   "mongodb+srv://admin:Yrskrmsr@cluster0.nwljkhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const DATABASE_NAME = "imagedb";
 let database;
+
+app.listen(PORT, () => {
+  MongoClient.connect(CONNECTION_STRING, (error, client) => {
+    if (error) {
+      console.error("Error connecting to MongoDB:", error);
+      return;
+    }
+    database = client.db(DATABASE_NAME);
+    console.log("MongoDB Connection Successful");
+  });
+});
+app.use(express.json());
