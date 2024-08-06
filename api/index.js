fd33f918662c33c9a6974cd9824ebcaf3117dd0b
PVSKR <skr.polu@hotmail.com>
2024-08-07 03:43:48 +0530

    i got but at delete member

--- api/index.js
+++ api/index.js
@@ -22,7 +22,9 @@ app.listen(PORT, () => {
     console.log("MongoDB Connection Successful");
   });
 });
+
 app.use(express.json());
+
 app.get("/api/familly/GetInfo", (req, res) => {
   database
     .collection("famillycollection")
@@ -30,65 +32,74 @@ app.get("/api/familly/GetInfo", (req, res) => {
     .toArray((error, result) => {
       if (error) {
         console.error("Error fetching Info:", error);
-        res.status(500), json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
         return;
       }
       res.json(result);
     });
 });
+
 app.post("/api/familly/AddMembers", multer().none(), (req, res) => {
-  const { name, work } = res.body;
+  const { name, work } = req.body;
   console.log("New member name:", name);
   console.log("New member work:", work);
+
   if (!name || !work) {
     res.status(400).json({ error: "Name and Work are required" });
     return;
   }
-  database.collection("famillycollection").countDocument({}, (error, count) => {
-    if (error) {
-      console.error("Error counting documents:", error);
-      res.status(500).json({ error: "Internal server error" });
-      return;
-    }
-    const newImageObject = {
-      id: (count + 1).toString(),
-      name: name,
-      salary: work,
-    };
-    database
-      .collection("famillycollection")
-      .insertOne(newImageObject, (error) => {
-        if (error) {
-          console.error("Error counting documents:", error);
-          res.status(500).json({ error: "Internal server error" });
-          return;
-        }
-        res.json({ message: "Add success fully" });
-      });
-  });
+
+  database
+    .collection("famillycollection")
+    .countDocuments({}, (error, count) => {
+      if (error) {
+        console.error("Error counting documents:", error);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+      const newMemberObject = {
+        id: (count + 1).toString(),
+        name: name,
+        work: work,
+      };
+      database
+        .collection("famillycollection")
+        .insertOne(newMemberObject, (error) => {
+          if (error) {
+            console.error("Error inserting document:", error);
+            res.status(500).json({ error: "Internal server error" });
+            return;
+          }
+          res.json({ message: "Added successfully" });
+        });
+    });
 });
+
 app.put("/api/familly/UpdateMembers", (req, res) => {
   const id = req.body.id;
   const { name, work } = req.body;
   const updatedMember = { name, work };
+
   database
     .collection("famillycollection")
     .updateOne({ id: id }, { $set: updatedMember }, (err, result) => {
       if (err) {
-        console.error("Error updating Image:", err);
+        console.error("Error updating document:", err);
         res.status(500).send("Internal server error");
         return;
       }
-      res.status(200).send("Member  updated successfully");
+      res.status(200).send("Member updated successfully");
     });
 });
+
 app.delete("/api/familly/DeleteMembers", (req, res) => {
   const memberId = req.query.id;
+
   database
     .collection("famillycollection")
     .deleteOne({ id: memberId }, (error) => {
       if (error) {
-        console.error("Error deleting Member:", error);
+        console.error("Error deleting document:", error);
         res.status(500).json({ error: "Internal server error" });
         return;
       }
